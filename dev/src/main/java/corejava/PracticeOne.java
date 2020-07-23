2d2aa8fd973e480f365b2b16beafac5d250f2446
Mastanvali Shaik <mastmdm@gmail.com>
2020-07-23 18:59:04 +0700

    first commit

---
+++ dev/src/main/java/corejava/PracticeOne.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package corejava;
+
+/**
+ * @author Mastanvali
+ *
+ */
+public class PracticeOne {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("All the best!");
+	}
+
+}
